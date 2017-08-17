c28439973f00667af38e822c3d75029f9d402f0d
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-17 09:12:45 -0400

    IsOrientRight working on shop page

--- webapp/viewmodel/shop.go
+++ webapp/viewmodel/shop.go
@@ -28,22 +28,25 @@ func NewShop() Shop {
 							organic juices that are guaranteed to be obtained from trees that
 							have never been treated with pesticides or artificial
 							fertilizers.`,
+		IsOrientRight: false,
 	}
 	supplyCategory := Category{
 		URL:      ".",
 		ImageURL: "kiwi.png",
 		Title:    "Cups, Straws, and Other Supplies",
 		Description: `From paper cups to bio-degradable plastic to straws and
-						napkins, LSS is your source for the sundries that keep your stand
-						running smoothly.`,
+							napkins, LSS is your source for the sundries that keep your stand
+							running smoothly.`,
+		IsOrientRight: true,
 	}
 	advertiseCategory := Category{
 		URL:      ".",
 		ImageURL: "pineapple.png",
 		Title:    "Signs and Advertising",
 		Description: `Sure, you could just wait for people to find your stand
-						along the side of the road, but if you want to take it to the next
-						level, our premium line of advertising supplies.`,
+							along the side of the road, but if you want to take it to the next
+							level, our premium line of advertising supplies.`,
+		IsOrientRight: false,
 	}
 	result.Categories = []Category{juiceCategory, supplyCategory, advertiseCategory} // combine together in slice
 	return result
